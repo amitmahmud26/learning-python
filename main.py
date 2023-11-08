abb7a52ddcb57846412f38f7996ac005e18da5bc
Amit Mahmud Sabbir <amitsclan26@gmail.com>
2023-11-09 04:40:54 +0600

    added guess game

--- main.py
+++ main.py
@@ -4,7 +4,67 @@
 # name = "John Smith"
 # age = 20
 # is_new_patient = True
+import math
 
-name = input("What is your name?")
-fav_color = input("Your favorite color?")
-print(name + ' likes ' + fav_color)
+# name = input("What is your name?")
+# fav_color = input("Your favorite color?")
+# print(name + ' likes ' + fav_color)
+
+# birth_year = 2000
+# age = 2023 - int(birth_year)
+# print(age)
+
+# converting pounds in kg
+
+# pounds = input("in pounds?")
+# kg = float(pounds) * 0.45
+# print(kg)
+
+# course = '''
+# Hi John!
+#     this is a three quote line i mean multiple line string
+# '''
+# # print(course)
+#
+# another = course[:]
+# another1 = course
+# print(type(another))
+# print(type(another1))
+
+# first = "Amit"
+# last = "Sabbir"
+#
+# full = f"{first} [{last}] is a coder"
+# print(full)
+# print(len(full))
+#
+# print(full.upper())
+# print(full.__len__())
+#
+# print(full.replace("Sabbir", "Mahmud"))
+#
+# print("Mahmud" in full)
+
+# total = 10 * 2**2 / 2
+# print(total)
+#
+# print(math.floor())
+
+# hot = False
+# cold = False
+#
+# if hot:
+#     print("Drink plenty of water")
+# elif cold:
+#     print("Wear warm cloths")
+# else:
+#     print("It's a wonderful day")
+#
+# print("Enjoy your day")
+
+# loops
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i += 1
