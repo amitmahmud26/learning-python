399b51f0c2c81aee6fb67fe5ef2501bfb8fb74b1
Amit Mahmud Sabbir <amitsclan26@gmail.com>
2023-11-08 21:41:37 +0600

    basic input

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+# print("hello world")
+# print ('*' * 10)
+#
+# name = "John Smith"
+# age = 20
+# is_new_patient = True
+
+name = input("What is your name?")
+fav_color = input("Your favorite color?")
+print(name + ' likes ' + fav_color)
