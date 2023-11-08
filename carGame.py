1d413e4880afd520f01d44b6eaf133305637286c
Amit Mahmud Sabbir <amitsclan26@gmail.com>
2023-11-09 05:43:14 +0600

    updated carGame

--- carGame.py
+++ carGame.py
@@ -1,10 +1,22 @@
 continuing = True
+start_count = 0
+stop_count = 0
 while continuing:
     inputed = input(">")
     if inputed.lower() == "start":
-        print("Car started... Ready to go")
+        start_count += 1
+        if start_count == 1:
+            print("Car started... Ready to go")
+            stop_count = 0
+        else:
+            print("Car is already started")
     elif inputed.lower() == "stop":
-        print("Car stopped")
+        stop_count += 1
+        if stop_count == 1:
+            print("Car stopped")
+            start_count = 0
+        else:
+            print("Car is already stopped")
     elif inputed.lower() == "quit":
         break
     else:
